5545d0bdb783cdd3e8b7b42cd35bddac7536ef6c
JuliaR <romasenkoy@mail.ru>
2025-10-24 21:36:14 +0300

    KS-1-3-ready Space

--- src/lesson_1/lesson1_task3.kt
+++ src/lesson_1/lesson1_task3.kt
@@ -1,5 +1,19 @@
 package lesson_1
 
 fun main () {
-    println("Решение урок 1 задание 3")
+    var year: Int = 1962
+    /*var hour: Int = 9
+    var minute: Int = 7*/
+
+    var hour: Int = 10
+    var minute: Int = 55
+
+    println(year)
+    /*println(hour)
+    println(minute)*/
+
+    print(hour)
+    print(":")
+    print(minute)
+
 }
