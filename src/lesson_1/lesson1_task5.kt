98caa5f7ab18613a966632eb4657c77406045274
JuliaR <romasenkoy@mail.ru>
2025-10-24 22:10:21 +0300

    KS-1-5-hard

--- src/lesson_1/lesson1_task5.kt
+++ src/lesson_1/lesson1_task5.kt
@@ -1,5 +1,19 @@
 package lesson_1
 
-fun main () {
+fun main() {
+    // Количество секунд
+    val Seconds = 6480  // 108 минут
 
+    // Часы
+    val hours = Seconds / 3600
+    //Минуты
+    val minutes = (Seconds % 3600) / 60
+    //Секунды
+    val seconds = Seconds % 60 // как вывести двумя нолями не понимаю
+
+    print(hours)
+    print(":")
+    print(minutes)
+    print(":")
+    print(seconds)
 }
