9ab5683c8f5171414c60aba23526713b5a1365de
JuliaR <romasenkoy@mail.ru>
2025-10-24 21:22:40 +0300

    KS-1-2-ready Workers

--- src/lesson_1/lesson1_task2.kt
+++ src/lesson_1/lesson1_task2.kt
@@ -1,6 +1,29 @@
 package lesson_1
 
+
 fun main () {
-    println("Решение урок 1 задание 1")
-    println("Доработка")
+
+    var OrderNumber: Int = 75
+    val ThanksText: String = "Thank you for order!"
+
+    println(OrderNumber)
+    println(ThanksText)
+
+    //Для начала выведи в консоль значения всех объявленных ранее переменных
+    println("Переменная 'OrderNumber' содержит в себе количество заказов")
+    println("Переменная 'ThanksText' содержит благодарственное сообщение ")
+
+    //Закомментируй предыдущий код
+   /* //количество работников данного интернет-магазина (2000). Присвоить и распечатать
+    var CountOfWorkers: Int = 2000
+    println("Количество сотрудников интернет-магазина равно: ")
+    println(CountOfWorkers)
+*/
+
+    //один работник уволился, присвой новое значение количества работников этой переменной и распечатай в консоль
+    var CountOfWorkers: Int = 1999
+    println("Количество сотрудников интернет-магазина равно: ")
+    println(CountOfWorkers)
+
+
 }
