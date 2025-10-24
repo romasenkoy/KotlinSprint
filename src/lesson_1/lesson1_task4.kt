9574cde3e99a2aa4295aeb905277b8f075b4433d
JuliaR <romasenkoy@mail.ru>
2025-10-24 21:55:23 +0300

    KS-1-4-ready types of variables

--- src/lesson_1/lesson1_task4.kt
+++ src/lesson_1/lesson1_task4.kt
@@ -1,5 +1,18 @@
 package lesson_1
 
 fun main () {
-    println("Решение урок 1 задание 3")
+
+    val length: Long  = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Double = 0.075
+    val seconds: Short = 6480
+    val partOfYear: Double =2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
 }
