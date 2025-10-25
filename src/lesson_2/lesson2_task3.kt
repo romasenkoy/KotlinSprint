2fbcf737dfa70f459296b506f0b3556592033c40
JuliaR <romasenkoy@mail.ru>
2025-10-25 23:49:09 +0300

    KS-2-3-trains ready

--- src/lesson_2/lesson2_task3.kt
+++ src/lesson_2/lesson2_task3.kt
@@ -1,9 +1,23 @@
 package lesson_2
 
-fun main () {
-    var OrderNumber: Int = 75
-    val ThanksText: String = "Thank you for order!"
+fun main() {
+    val StartHour = 9
+    val StartMinute = 39
+    val TravelTime = 457
 
-    println(OrderNumber)
-    println(ThanksText)
+    // Отправление с начала дня переводим в минуты
+    val DepartureInMinutes = StartHour * 60 + StartMinute
+
+    // Время прибытия в минутах с начала дня
+    val ArrivalInMinutes = DepartureInMinutes + TravelTime
+
+    // Час и минуты прибытия
+    val ArrivalHour = ArrivalInMinutes / 60
+    val ArrivalMinute = ArrivalInMinutes % 60
+
+    print("Время прибытия - ")
+    print(ArrivalHour)
+    print(":")
+    print(ArrivalMinute)
 }
+
