4bf4913f70cc83daefa001e1338451a76b8569e2
JuliaR <romasenkoy@mail.ru>
2025-10-26 19:56:55 +0300

    KS-1-3-Space  correcteв-on-26.10.2025

--- src/lesson_1/lesson1_task3.kt
+++ src/lesson_1/lesson1_task3.kt
@@ -1,16 +1,16 @@
 package lesson_1
 
 fun main () {
-    val year: Int = 1962
-    var hour: Int = 9
+    val year: String = "1962"
+    var hour: String = "9"
     var minute:String = "07" // тут только возник вопрос, если эта переменная с типом String, то остальные можно оставить Int или тоже сделать String?
 
     println(year)
     println(hour)
     println(minute)
 
-    hour = 10
-    minute = 55.toString()
+    hour = "10"
+    minute = "55"
 
     print(hour)
     print(":")
