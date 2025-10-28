564bbfa0fa3101a8d804ff6ac0a5548fbbc3aca6
romasenkoy <romasenkou@gmail.com>
2025-10-28 18:32:45 +0300

    Revert "KS-3-4-chess ready"

--- src/lesson_3/lesson3_task4.kt
+++ src/lesson_3/lesson3_task4.kt
@@ -1,21 +1,5 @@
 package lesson_3
 
 fun main() {
-    // первый ход
-    var from = "E2"
-    var to = "E4"
-    var moveNumber = 1
 
-    // вывод первого хода
-    var moveString = "[$from-$to;$moveNumber]"
-    println(moveString)
-
-    // второй хода
-    from = "D2"
-    to = "D3"
-    moveNumber = 2
-
-    // вывод второго хода
-    moveString = "[" + from + "-" + to + ";" + moveNumber + "]"
-    println(moveString)
 }
