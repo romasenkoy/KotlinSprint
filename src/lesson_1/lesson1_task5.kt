f29ae7c301673457164c2d8fa3271f5860994b10
JuliaR <romasenkoy@mail.ru>
2025-10-28 18:25:49 +0300

    KS-1-5-hard corrected-on-28.10.2025

--- src/lesson_1/lesson1_task5.kt
+++ src/lesson_1/lesson1_task5.kt
@@ -1,5 +1,6 @@
 package lesson_1
 
+/*
 fun main() {
     // Количество секунд
     val Seconds = 6480  // это 108 минут
@@ -16,5 +17,17 @@ fun main() {
     print(minutes)
     print(":")
     print("00")
+}*/
 
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
+
+fun main() {
+    val allSeconds = 10800  // общее количество секунд
+
+    val hours = allSeconds / SECONDS_IN_HOUR
+    val minutes = (allSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val seconds = allSeconds % SECONDS_IN_MINUTE
+
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
