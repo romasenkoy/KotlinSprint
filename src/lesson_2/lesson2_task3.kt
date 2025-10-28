28ee3ef9a874fa12ab6e97bfd45913dc9d668cb7
JuliaR <romasenkoy@mail.ru>
2025-10-28 18:17:56 +0300

    KS-2-1-trains corrected-on-28.10.2025

--- src/lesson_2/lesson2_task3.kt
+++ src/lesson_2/lesson2_task3.kt
@@ -1,20 +1,22 @@
 package lesson_2
 
+const val MINUTES_IN_HOUR = 60
+
 fun main() {
     val startHour = 9
     val startMinute = 39
     val travelTime = 457
-    val minutesInHour = 60 //вынесла в константу
+    // val minutesInHour = 60 //вынесла в константу
 
     // Отправление с начала дня переводим в минуты
-    val departureInMinutes = startHour * minutesInHour + startMinute
+    val departureInMinutes = startHour * MINUTES_IN_HOUR + startMinute
 
     // Время прибытия в минутах с начала дня
     val arrivalInMinutes = departureInMinutes + travelTime
 
     // Час и минуты прибытия
-    val arrivalHour = arrivalInMinutes / 60
-    val arrivalMinute = arrivalInMinutes % 60
+    val arrivalHour = arrivalInMinutes / MINUTES_IN_HOUR
+    val arrivalMinute = arrivalInMinutes % MINUTES_IN_HOUR
 
     print("Время прибытия - ")
     print(arrivalHour)
