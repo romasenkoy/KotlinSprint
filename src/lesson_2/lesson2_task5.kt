2b6d2a954a3272169fb8e936e6f50d6de317c66c
JuliaR <romasenkoy@mail.ru>
2025-10-28 18:40:09 +0300

    KS-2-5-hard-bank ready

--- src/lesson_2/lesson2_task5.kt
+++ src/lesson_2/lesson2_task5.kt
@@ -1,14 +1,12 @@
 package lesson_2
 
 fun main() {
-    val initialAmount = 70_000.0      // начальная сумма
-    val interestRate = 0.167          // 16.7%
-    val years = 20                    // количество лет
+    val initialAmount = 70_000.0
+    val interestRate = 0.167
+    val years = 20
 
-    // Формула сложных процентов
     val finalAmount = initialAmount * Math.pow(1 + interestRate, years.toDouble())
 
-    // Вывод с 3 знаками после запятой
     println(String.format("%.3f", finalAmount))
 
 }
